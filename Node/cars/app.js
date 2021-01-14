2c548dfa85e841672f07cfc5a7bc55b580f09596
Emad <emad998@yahoo.com>
2021-01-14 11:16:36 -0800

    added node assignments

---
+++ Node/cars/app.js
@@ -0,0 +1,61 @@
+const http = require('http');
+const fs = require('fs');
+const server = http.createServer(function (request, response){
+    console.log('client request URL: ', request.url);
+    
+    // this is how we do routing:
+    if(request.url === '/cars') {
+        fs.readFile('views/cars.html', 'utf8', function (errors, contents){
+            response.writeHead(200, {'Content-Type': 'text/html'});  // send data about response
+            response.write(contents);  //  send response body
+            response.end(); // finished!
+        });
+    } else if(request.url === '/images/mercedes.jpg') {
+        fs.readFile('images/mercedes.jpg', function (errors, contents){
+            response.writeHead(200, {'Content-Type': 'image/jpg'});  // send data about response
+            response.write(contents);  //  send response body
+            response.end(); // finished!
+        });
+    } else if(request.url === '/stylesheets/mainStyles.css') {
+        fs.readFile('stylesheets/mainStyles.css', 'utf8', function (errors, contents){
+            response.writeHead(200, {'Content-Type': 'text/css'});  // send data about response
+            response.write(contents);  //  send response body
+            response.end(); // finished!
+        });
+    }  else if(request.url === '/cats') {
+        fs.readFile('views/cats.html', 'utf8', function (errors, contents){
+            response.writeHead(200, {'Content-Type': 'text/html'});  // send data about response
+            response.write(contents);  //  send response body
+            response.end(); // finished!
+        });
+    } else if(request.url === '/images/cuteCat.jpg') {
+        fs.readFile('images/cuteCat.jpg', function (errors, contents){
+            response.writeHead(200, {'Content-Type': 'image/jpg'});  // send data about response
+            response.write(contents);  //  send response body
+            response.end(); // finished!
+        });
+    } else if(request.url === '/stylesheets/catStyles.css') {
+        fs.readFile('stylesheets/catStyles.css', 'utf8', function (errors, contents){
+            response.writeHead(200, {'Content-Type': 'text/css'});  // send data about response
+            response.write(contents);  //  send response body
+            response.end(); // finished!
+        });
+    } else if(request.url === '/cars/new') {
+        fs.readFile('views/carForm.html', 'utf8', function (errors, contents){
+            response.writeHead(200, {'Content-Type': 'text/html'});  // send data about response
+            response.write(contents);  //  send response body
+            response.end(); // finished!
+        });
+    } 
+
+
+    // request didn't match anything:
+    else {
+        response.writeHead(404);
+        response.end('File not found!!!');
+    }
+});
+// tell your server which port to run on
+server.listen(6789);
+// print to terminal window
+console.log("Running in localhost at port 6789");
